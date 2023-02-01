4e025112a72e91b8b3f16ff49489617b304bdb42
Leroy Steding <leroysteding@gmail.com>
2023-02-01 20:57:21 +0100

    fix

--- medusa-config.js
+++ medusa-config.js
@@ -56,13 +56,14 @@ const plugins = [
 
 module.exports = {
   projectConfig: {
-    projectConfig: {
-    redis_url: REDIS_URL,
-    database_url: DATABASE_URL,
-    database_type: "postgres",
+    // redis_url: REDIS_URL,
+    // For more production-like environment install PostgresQL
+    // database_url: DATABASE_URL,
+    // database_type: "postgres",
+    database_database: "./medusa-db.sql",
+    database_type: "sqlite",
     store_cors: STORE_CORS,
     admin_cors: ADMIN_CORS,
-  },
   },
   plugins,
 };
