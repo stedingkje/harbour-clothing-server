cbe829d2e04a85afff43b91ab711618a6a55fa38
Leroy Steding <leroysteding@gmail.com>
2023-02-02 11:57:47 +0100

    config changes

--- medusa-config.js
+++ medusa-config.js
@@ -55,15 +55,18 @@ const plugins = [
 ];
 
 module.exports = {
-  projectConfig: {
-    // redis_url: REDIS_URL,
+   projectConfig: {
+    redis_url: REDIS_URL,
     // For more production-like environment install PostgresQL
-    // database_url: DATABASE_URL,
-    // database_type: "postgres",
-    database_database: "./medusa-db.sql",
-    database_type: "sqlite",
+    database_url: DATABASE_URL,
+    database_type: "postgres",
+    // database_database: "./medusa-db.sql",
+    // database_type: "sqlite",
     store_cors: STORE_CORS,
     admin_cors: ADMIN_CORS,
+    // ...(process.env.NODE_ENV === "production" && {
+    //   database_extra: { ssl: { rejectUnauthorized: false } },
+    // }),
   },
   plugins,
 };
